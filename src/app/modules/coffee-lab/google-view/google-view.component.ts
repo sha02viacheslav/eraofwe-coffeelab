1bb9fb278f58fed072af08b523a297aad1e1180e
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-03-19 14:37:30 +0400

    updated google-view

--- src/app/modules/coffee-lab/google-view/google-view.component.ts
+++ src/app/modules/coffee-lab/google-view/google-view.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-google-view',
-  templateUrl: './google-view.component.html',
-  styleUrls: ['./google-view.component.scss']
+    selector: 'app-google-view',
+    templateUrl: './google-view.component.html',
+    styleUrls: ['./google-view.component.scss'],
 })
 export class GoogleViewComponent implements OnInit {
+    forumKeySearch: string;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
