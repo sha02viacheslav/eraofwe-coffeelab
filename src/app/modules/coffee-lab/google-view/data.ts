44fb2f77c5068f26f88695aa18a2a3218ac9c8bd
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-03-21 16:22:26 +0400

    updated google view

--- src/app/modules/coffee-lab/google-view/data.ts
+++ src/app/modules/coffee-lab/google-view/data.ts
@@ -20,6 +20,14 @@ export const DISCUSSIONS_FORUM = [
                 description:
                     'Coffee provides a complex blend of different flavours, which together produce a range of sensory experiences. The sensory profile of a cup of coffee varies according to a number of factors: the type and blend of coffee beans.Coffee provides a complex blend of different flavours, which together produce a range of sensory experiences. The sensory profile of a cup of coffee varies according to a number of factors: the type and blend of coffee beans; geographical source; roasting method; and method of preparation8,9.  The variation in these aspects will impact the overall sensory experience obtained from a cup of coffee during both the preparation and consumption of the coffee.',
             },
+            {
+                author: 'Gabriel Match',
+                nickname: 'Coffee Hero',
+                info: 'Third wave coffee roaster',
+                date: '3 days ago',
+                description:
+                    'Coffee provides a complex blend of different flavours, which together produce a range of sensory experiences. The sensory profile of a cup of coffee varies according to a number of factors: the type and blend of coffee beans.Coffee provides a complex blend of different flavours, which together produce a range of sensory experiences. The sensory profile of a cup of coffee varies according to a number of factors: the type and blend of coffee beans; geographical source; roasting method; and method of preparation8,9.  The variation in these aspects will impact the overall sensory experience obtained from a cup of coffee during both the preparation and consumption of the coffee.',
+            },
         ],
     },
     {
