483445cef779706cfcc7189ac7d91c9c53cbf975
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-04-07 14:42:49 +0400

    fixed

--- src/app/modules/coffee-lab/components/bottom-banner/bottom-banner.component.ts
+++ src/app/modules/coffee-lab/components/bottom-banner/bottom-banner.component.ts
@@ -6,30 +6,20 @@ import { Component, OnInit, HostListener } from '@angular/core';
     styleUrls: ['./bottom-banner.component.scss'],
 })
 export class BottomBannerComponent implements OnInit {
-    lastScrollTop = 0;
     showBanner = false;
-    isDouble = false;
 
     @HostListener('window:scroll', ['$event'])
     onWindowScroll(event) {
         event.preventDefault();
         const pos = document.documentElement.scrollTop || document.body.scrollTop;
-        if (pos > this.lastScrollTop) {
-            if (this.showBanner && !this.isDouble) {
-                setTimeout(() => {
-                    this.isDouble = true;
-                    window.scrollTo(0, 1);
-                }, 500);
-            } else {
-                this.showBanner = true;
-                this.isDouble = false;
-            }
+        if (pos >= 300) {
+            this.showBanner = true;
+            setTimeout(() => {
+                window.scrollTo(0, 300);
+            }, 100);
         } else {
-            if (!this.isDouble || pos === 0) {
-                this.showBanner = false;
-            }
+            this.showBanner = false;
         }
-        this.lastScrollTop = pos <= 0 ? 0 : pos;
     }
 
     constructor() {}
