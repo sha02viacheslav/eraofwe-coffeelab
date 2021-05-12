3bc8459852606c661d9c947dab671ae8ca11eb5b
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-05-12 20:42:03 +0400

    Fixed angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
@@ -11,6 +12,8 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HealthCheckComponent } from '@components';
+
+import { environment } from '@env/environment';
 
 @NgModule({
     declarations: [AppComponent, LayoutComponent, HealthCheckComponent],
@@ -32,6 +35,7 @@ import { HealthCheckComponent } from '@components';
                 deps: [HttpClient],
             },
         }),
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     ],
     providers: [],
     bootstrap: [AppComponent],
