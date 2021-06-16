de0419411e81e377a0967f753841a82a10060aaa
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-06-16 22:43:36 +0400

    Testing SEO

--- src/app/modules/coffee-lab/google-view/articles/article-detail/article-detail.component.ts
+++ src/app/modules/coffee-lab/google-view/articles/article-detail/article-detail.component.ts
@@ -30,110 +30,16 @@ export class ArticleDetailComponent implements OnInit {
         private startupService: StartupService,
         private globalsService: GlobalsService,
         @Inject(DOCUMENT) private doc,
-    ) {
-        this.setSEO();
-        this.activatedRoute.queryParams.subscribe((params) => {
-            this.isPublic = params.is_public;
-        });
-        this.activatedRoute.params.subscribe((params) => {
-            if (params.idOrSlug) {
-                this.idOrSlug = params.idOrSlug;
-                this.getDetails();
-            }
-            if (!this.relatedData?.length) {
-                this.getArticleList();
-            }
-        });
-    }
+    ) {}
 
     ngOnInit(): void {
-        this.previousUrl = this.globalsService.previousUrl;
-    }
-
-    getArticleList() {
-        this.coffeeLabService.getForumList('article').subscribe((res: any) => {
-            if (res.success) {
-                this.relatedData = res.result
-                    .filter((item) => item.id !== this.idOrSlug && item.slug !== this.idOrSlug)
-                    .slice(0, 3);
-                if (!this.idOrSlug) {
-                    this.router.navigate([`${this.relatedData[0].language}/article/${this.relatedData[0].slug}`]);
-                }
-            }
-        });
-    }
-
-    getDetails() {
-        this.loading = true;
-        this.coffeeLabService.getForumDetails('article', this.idOrSlug).subscribe((res: any) => {
-            if (res.success) {
-                this.detailsData = res.result;
-                this.lang = res.result.language;
-                if (!this.isPublic) {
-                    this.globalsService.setLimitCounter();
-                }
-                this.startupService.load(this.lang || 'en');
-                this.setSEO();
-                this.setSchemaMackup();
-            } else {
-                this.toastService.error('The article is not exist.');
-                this.router.navigate(['/error']);
-            }
-            this.loading = false;
-        });
+        this.setSEO();
     }
 
     setSEO() {
-        this.seoService.setPageTitle(this.detailsData?.title || 'Era of We - The Coffee Lab');
-        this.seoService.setMetaData(
-            'description',
-            this.detailsData?.content
-                ? this.globalsService.getJustText(this.detailsData?.content)
-                : 'article for Coffee',
-        );
+        this.seoService.setPageTitle('Era of We - The Coffee Lab');
+        this.seoService.setMetaData('description', 'article for Coffee');
         this.seoService.createLinkForCanonicalURL();
         this.seoService.createLinkForHreflang(this.lang || 'x-default');
-    }
-
-    setSchemaMackup() {
-        this.jsonLD = {
-            '@context': 'https://schema.org',
-            '@graph': [
-                {
-                    '@type': 'BreadcrumbList',
-                    itemListElement: [
-                        {
-                            '@type': 'ListItem',
-                            position: 1,
-                            name: 'Overview',
-                            item: `${environment.coffeeLabWeb}/${this.lang}/overview`,
-                        },
-                        {
-                            '@type': 'ListItem',
-                            position: 2,
-                            name: 'Posts',
-                            item: `${environment.coffeeLabWeb}/${this.lang}/overview/articles`,
-                        },
-                        {
-                            '@type': 'ListItem',
-                            position: 3,
-                            name: this.detailsData?.title,
-                        },
-                    ],
-                },
-                {
-                    '@type': 'Article',
-                    '@id': this.doc.URL,
-                    headline: this.seoService.getPageTitle(),
-                    description: this.globalsService.getJustText(this.detailsData?.content),
-                    image: this.detailsData?.cover_image_url,
-                    datePublished: this.detailsData?.created_at,
-                    author: {
-                        '@type': 'Person',
-                        name: this.detailsData.user_name,
-                    },
-                },
-            ],
-        };
     }
 }
