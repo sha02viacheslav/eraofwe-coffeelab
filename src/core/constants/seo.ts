251fead2f2e61eab14244ee1cf6710f346201828
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-06-21 15:54:27 +0400

    Updated preview image

--- src/core/constants/seo.ts
+++ src/core/constants/seo.ts
@@ -1,5 +1,4 @@
 export const seoVariables = {
     author: 'EraOfWe',
-    image:
-        'https://bed-api.sewnstaging.com/public/staging/file_manager/cl-articles/5_4ENYVY20210611180621832913822.png',
+    image: 'assets/images/eow-logo.png',
 };
