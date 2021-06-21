b8b568d1b60d0752c853eb156e82884c3d9b4775
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-06-21 15:24:00 +0400

    Test preview

--- src/core/constants/seo.ts
+++ src/core/constants/seo.ts
@@ -1,4 +1,5 @@
 export const seoVariables = {
     author: 'EraOfWe',
-    image: 'https://www.eraofwe.com/static/white-logo-5aa38fc026432a752e125578383487a4.svg',
+    image:
+        'https://bed-api.sewnstaging.com/public/staging/file_manager/cl-articles/5_4ENYVY20210611180621832913822.png',
 };
