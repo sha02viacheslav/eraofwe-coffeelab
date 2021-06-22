a2e3956e906184aff2f8ce45ab037198a0388453
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-06-22 17:43:07 +0400

    Updated link image

--- src/app/modules/coffee-lab/google-view/articles/articles-view/articles-view.component.ts
+++ src/app/modules/coffee-lab/google-view/articles/articles-view/articles-view.component.ts
@@ -170,7 +170,9 @@ export class ArticlesViewComponent extends ResizeableComponent implements OnInit
         this.seoService.setMetaData('property', 'og:title', title);
         this.seoService.setMetaData('property', 'og:description', description);
         this.seoService.setMetaData('property', 'og:url', this.document.URL);
+        this.seoService.setMetaData('property', 'og:image', seoVariables.image);
 
+        this.seoService.setMetaData('name', 'twitter:image', seoVariables.image);
         this.seoService.setMetaData('name', 'twitter:creator', seoVariables.author);
         this.seoService.setMetaData('name', 'twitter:site', this.document.URL);
         this.seoService.setMetaData('name', 'twitter:title', title);
--- src/app/modules/coffee-lab/google-view/era-of-we/era-of-we.component.ts
+++ src/app/modules/coffee-lab/google-view/era-of-we/era-of-we.component.ts
@@ -68,7 +68,9 @@ export class EraOfWeComponent implements OnInit, OnDestroy {
         this.seoService.setMetaData('property', 'og:title', title);
         this.seoService.setMetaData('property', 'og:description', description);
         this.seoService.setMetaData('property', 'og:url', this.document.URL);
+        this.seoService.setMetaData('property', 'og:image', seoVariables.image);
 
+        this.seoService.setMetaData('name', 'twitter:image', seoVariables.image);
         this.seoService.setMetaData('name', 'twitter:creator', seoVariables.author);
         this.seoService.setMetaData('name', 'twitter:site', this.document.URL);
         this.seoService.setMetaData('name', 'twitter:title', title);
