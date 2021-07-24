090afcd723b8c284eefb49ecdf7f74631ceae880
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-07-24 04:36:28 +0300

    fix forum link

--- src/app/modules/coffee-lab/components/join-community/join-community.component.ts
+++ src/app/modules/coffee-lab/components/join-community/join-community.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { environment } from '@env/environment';
 import { CoffeeLabService } from '@services';
+import { routerMap } from '@constants';
 
 @Component({
     selector: 'app-join-community',
@@ -9,6 +10,7 @@ import { CoffeeLabService } from '@services';
     styleUrls: ['./join-community.component.scss'],
 })
 export class JoinCommunityComponent implements OnInit {
+    readonly routerMap = routerMap;
     ssoWeb = environment.ssoWeb;
     isStaging = environment.needProtect;
     constructor(@Inject(DOCUMENT) private document: Document, public coffeeLabService: CoffeeLabService) {}
