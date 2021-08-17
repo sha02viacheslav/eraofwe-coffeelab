6dfeebe9579852c6ef9270d00d07fd8ec2db302c
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-08-17 18:51:32 +0530

    related discussion issue fixed

--- src/app/modules/coffee-lab/components/join-community/join-community.component.ts
+++ src/app/modules/coffee-lab/components/join-community/join-community.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Input } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { environment } from '@env/environment';
 import { CoffeeLabService, I18NService } from '@services';
 import { RouterMap } from '@constants';
 import { RouterSlug } from '@enums';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-join-community',
@@ -14,33 +15,45 @@ export class JoinCommunityComponent implements OnInit {
     readonly RouterMap = RouterMap;
     readonly RouterSlug = RouterSlug;
     ssoWeb = environment.ssoWeb;
+    @Input() pages: any;
+    @Input() type: any;
+    idOrSlug: any;
     isStaging = environment.needProtect;
-    questionList = [
-        {
-            question:
-                'Hey, could someone explain to me more about Omni roasts and the benefits from a business (Roaster) perspective?',
-        },
-        {
-            question:
-                'Hi Everyone, Our family have estates at Sumatera, Indonesia. Is there any buyers here who like to try our sample?',
-        },
-        {
-            question: 'What are coffee flavour profiles? How many profiles can a coffee have?',
-        },
-        {
-            question: 'What are coffee flavour profiles? How many profiles can a coffee have?',
-        },
-        {
-            question: 'What are coffee flavour profiles? How many profiles can a coffee have?',
-        },
-    ];
-    constructor(
-        @Inject(DOCUMENT) private document: Document,
-        public coffeeLabService: CoffeeLabService,
-        public i18nService: I18NService,
-    ) {}
+    relatedData = [];
+    constructor(@Inject(DOCUMENT) private document: Document, public coffeeLabService: CoffeeLabService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.getQaList();
+    }
+
+    getQaList() {
+        this.coffeeLabService
+            .getForumList(this.type, {
+                page: this.pages ? this.pages + 1 : 2,
+                per_page: 5,
+            })
+            .subscribe((res: any) => {
+                console.log(res);
+                if (res.success) {
+                    if (this.type === 'question') {
+                        this.relatedData = res.result.questions;
+                    } else {
+                        this.relatedData = res.result;
+                    }
+                }
+            });
+    }
+
+    getLink(item: any, answer: any) {
+        const type = this.type === 'question' ? 'qa-forum' : 'articles';
+        const url = `/${this.coffeeLabService.currentForumLanguage}/${type}/${item.slug}`;
+        return {
+            url,
+            queryParmas: {
+                answer: answer?.id,
+            },
+        };
+    }
 
     gotoSignup() {
         this.document.location.href = `${environment.ssoWeb}/sign-up`;
