67f4e3022306f9bb5d0788d3de9583e9b6faf3fc
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-09-06 17:02:41 +0300

    remove bugherd

--- src/app/modules/coffee-lab/components/coffee-lab-components.module.ts
+++ src/app/modules/coffee-lab/components/coffee-lab-components.module.ts
@@ -24,7 +24,7 @@ const COMPONENTS = [
     JsonLdComponent,
     LanguageDropdownComponent,
     LikeDividerComponent,
-    LimitBannerComponent,
+    // LimitBannerComponent,
     PublishForumComponent,
     SearchForumComponent,
     SignupModalComponent,
