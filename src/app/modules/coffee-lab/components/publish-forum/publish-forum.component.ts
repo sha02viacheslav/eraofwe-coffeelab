51d75cb9c40ad8f192e1f3f41e86d701222068ea
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-09-13 17:26:18 +0530

    bug fix

--- src/app/modules/coffee-lab/components/publish-forum/publish-forum.component.ts
+++ src/app/modules/coffee-lab/components/publish-forum/publish-forum.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { GlobalsService } from '@services';
+import { CoffeeLabService, GlobalsService } from '@services';
 import { DialogService } from 'primeng/dynamicdialog';
 import { SignupModalComponent } from '../signup-modal/signup-modal.component';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'app-publish-forum',
@@ -13,22 +15,29 @@ export class PublishForumComponent implements OnInit {
     firstBtnValue: string;
     secondBtnValue: string;
     placeHolderValue: string;
-    constructor(public dialogSrv: DialogService, private globals: GlobalsService) {}
+    destroy$: Subject<boolean> = new Subject<boolean>();
+    constructor(
+        public dialogSrv: DialogService,
+        private globals: GlobalsService,
+        private coffeeLabService: CoffeeLabService,
+    ) {}
 
     ngOnInit(): void {
-        if (this.type === 'article') {
-            this.firstBtnValue = this.globals.languageJson?.ask_a_question;
-            this.secondBtnValue = this.globals.languageJson?.create_brew_guide;
-            this.placeHolderValue = this.globals.languageJson?.write_post;
-        } else if (this.type === 'recipe') {
-            this.firstBtnValue = this.globals.languageJson?.ask_a_question;
-            this.secondBtnValue = this.globals.languageJson?.write_post;
-            this.placeHolderValue = this.globals.languageJson?.create_brew_guide;
-        } else {
-            this.firstBtnValue = this.globals.languageJson?.write_post;
-            this.secondBtnValue = this.globals.languageJson?.create_brew_guide;
-            this.placeHolderValue = this.globals.languageJson?.ask_your_question;
-        }
+        this.coffeeLabService.gotTranslations.pipe(takeUntil(this.destroy$)).subscribe((language) => {
+            if (this.type === 'article') {
+                this.firstBtnValue = this.globals.languageJson?.ask_a_question;
+                this.secondBtnValue = this.globals.languageJson?.create_brew_guide;
+                this.placeHolderValue = this.globals.languageJson?.write_post;
+            } else if (this.type === 'recipe') {
+                this.firstBtnValue = this.globals.languageJson?.ask_a_question;
+                this.secondBtnValue = this.globals.languageJson?.write_post;
+                this.placeHolderValue = this.globals.languageJson?.create_brew_guide;
+            } else {
+                this.firstBtnValue = this.globals.languageJson?.write_post;
+                this.secondBtnValue = this.globals.languageJson?.create_brew_guide;
+                this.placeHolderValue = this.globals.languageJson?.ask_your_question;
+            }
+        });
     }
 
     onFocus() {
