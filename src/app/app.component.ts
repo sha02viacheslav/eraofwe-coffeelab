f41c89c420ad02cd47b166d9869d6c5e79895ca5
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-09-23 10:24:08 +0300

    remove delay of dynamic scripts in staging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,27 +28,30 @@ export class AppComponent {
 
     setDynamicScripts() {
         if (isPlatformBrowser(this.platformId)) {
-            setTimeout(() => {
-                const dynamicScripts = [];
-                if (environment.production) {
-                    if (environment.needBugherd) {
-                        dynamicScripts.push('https://www.bugherd.com/sidebarv2.js?apikey=y5kbdd1ahghgywnjmyw7lg');
+            setTimeout(
+                () => {
+                    const dynamicScripts = [];
+                    if (environment.production) {
+                        if (environment.needBugherd) {
+                            dynamicScripts.push('https://www.bugherd.com/sidebarv2.js?apikey=y5kbdd1ahghgywnjmyw7lg');
+                        }
+                        dynamicScripts.push(
+                            'https://cmp.osano.com/6olZFSThsdZWqs/d3243605-8fd0-446a-9b25-a172e9ae3d67/osano.js',
+                        );
+                        dynamicScripts.push('https://jscloud.net/x/13391/inlinks.js');
+                        dynamicScripts.push('https://www.googletagmanager.com/gtm.js?id=GTM-KZWL6RR');
                     }
-                    dynamicScripts.push(
-                        'https://cmp.osano.com/6olZFSThsdZWqs/d3243605-8fd0-446a-9b25-a172e9ae3d67/osano.js',
-                    );
-                    dynamicScripts.push('https://jscloud.net/x/13391/inlinks.js');
-                    dynamicScripts.push('https://www.googletagmanager.com/gtm.js?id=GTM-KZWL6RR');
-                }
 
-                for (const value of dynamicScripts) {
-                    const node = this.document.createElement('script');
-                    node.src = value;
-                    node.type = 'text/javascript';
-                    node.async = true;
-                    this.document.getElementsByTagName('head')[0].appendChild(node);
-                }
-            }, 7000);
+                    for (const value of dynamicScripts) {
+                        const node = this.document.createElement('script');
+                        node.src = value;
+                        node.type = 'text/javascript';
+                        node.async = true;
+                        this.document.getElementsByTagName('head')[0].appendChild(node);
+                    }
+                },
+                this.isStaging ? 0 : 7000,
+            );
         }
     }
 }
