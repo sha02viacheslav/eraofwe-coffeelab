0f02aca24e892be555d8ee48437cd6055a74ca70
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-09-28 15:05:22 +0300

    fix search forum component

--- src/app/modules/coffee-lab/components/search-forum/search-forum.component.ts
+++ src/app/modules/coffee-lab/components/search-forum/search-forum.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CoffeeLabService } from '@services';
 import { DialogService } from 'primeng/dynamicdialog';
 import { SignupModalComponent } from '../signup-modal/signup-modal.component';
 
@@ -9,10 +8,7 @@ import { SignupModalComponent } from '../signup-modal/signup-modal.component';
     styleUrls: ['./search-forum.component.scss'],
 })
 export class SearchForumComponent implements OnInit {
-    forumKeySearch: string;
-    showDialog = false;
-
-    constructor(public dialogSrv: DialogService, public coffeeLabService: CoffeeLabService) {}
+    constructor(public dialogSrv: DialogService) {}
 
     ngOnInit(): void {}
 
