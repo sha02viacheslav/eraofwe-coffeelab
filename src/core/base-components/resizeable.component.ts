4b3f6967222d2c045fad09265bf5456a9b06de48
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-10-03 09:37:50 +0300

    fix resize service

--- src/core/base-components/resizeable.component.ts
+++ src/core/base-components/resizeable.component.ts
@@ -1,20 +1,14 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { ResizeService } from '@services';
-import { takeUntil } from 'rxjs/operators';
 import { DestroyableComponent } from './destroyable.component';
 
 @Component({
     template: '',
 })
 export class ResizeableComponent extends DestroyableComponent {
-    isMobile$ = this.resizeService.isMobile$.pipe(takeUntil(this.unsubscribeAll$));
-    isTablet$ = this.resizeService.isTablet$.pipe(takeUntil(this.unsubscribeAll$));
-    isDesktop$ = this.resizeService.isDesktop$.pipe(takeUntil(this.unsubscribeAll$));
-
-    @HostListener('window:resize', ['$event'])
-    onResize(event: any) {
-        this.resizeService.onResize(event.currentTarget.innerWidth);
-    }
+    isMobile$ = this.resizeService.isMobile$;
+    isTablet$ = this.resizeService.isTablet$;
+    isDesktop$ = this.resizeService.isDesktop$;
 
     constructor(protected resizeService: ResizeService) {
         super();
--- src/core/services/resize/resize.service.ts
+++ src/core/services/resize/resize.service.ts
@@ -1,7 +1,5 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -10,47 +8,33 @@ export class ResizeService {
     private readonly tabletMinWidth = 768;
     private readonly desktopMinWidth = 992;
 
-    private readonly isMobileSubject = new BehaviorSubject<boolean>(
-        isPlatformBrowser(this.platformId) ? window.innerWidth < this.tabletMinWidth : false,
-    );
-    private readonly isTabletSubject = new BehaviorSubject<boolean>(
-        isPlatformBrowser(this.platformId)
-            ? window.innerWidth < this.desktopMinWidth && window.innerWidth >= this.tabletMinWidth
-            : false,
-    );
-    private readonly isDesktopSubject = new BehaviorSubject<boolean>(
-        isPlatformBrowser(this.platformId) ? window.innerWidth >= this.desktopMinWidth : true,
-    );
+    isMobile$ = isPlatformBrowser(this.platformId) ? window.innerWidth < this.tabletMinWidth : false;
+    isTablet$ = isPlatformBrowser(this.platformId)
+        ? window.innerWidth < this.desktopMinWidth && window.innerWidth >= this.tabletMinWidth
+        : false;
+    isDesktop$ = isPlatformBrowser(this.platformId) ? window.innerWidth >= this.desktopMinWidth : true;
 
-    get isMobile$(): Observable<boolean> {
-        return this.isMobileSubject.asObservable().pipe(distinctUntilChanged());
+    constructor(@Inject(PLATFORM_ID) private platformId: object) {
+        if (isPlatformBrowser(this.platformId)) {
+            this.onResize(window.innerWidth);
+        }
     }
-
-    get isTablet$(): Observable<boolean> {
-        return this.isTabletSubject.asObservable().pipe(distinctUntilChanged());
-    }
-
-    get isDesktop$(): Observable<boolean> {
-        return this.isDesktopSubject.asObservable().pipe(distinctUntilChanged());
-    }
-
-    constructor(@Inject(PLATFORM_ID) private platformId: object) {}
 
     isMobile(): boolean {
-        return this.isMobileSubject.getValue();
+        return this.isMobile$;
     }
 
     isTablet(): boolean {
-        return this.isTabletSubject.getValue();
+        return this.isTablet$;
     }
 
     isDesktop(): boolean {
-        return this.isDesktopSubject.getValue();
+        return this.isDesktop$;
     }
 
     onResize(width: number): void {
-        this.isMobileSubject.next(width < this.tabletMinWidth);
-        this.isTabletSubject.next(width < this.desktopMinWidth && width >= this.tabletMinWidth);
-        this.isDesktopSubject.next(width >= this.desktopMinWidth);
+        this.isMobile$ = width < this.tabletMinWidth;
+        this.isTablet$ = width < this.desktopMinWidth && width >= this.tabletMinWidth;
+        this.isDesktop$ = width >= this.desktopMinWidth;
     }
 }
