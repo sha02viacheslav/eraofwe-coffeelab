370ca5909b72d2f0e607ea8b755ea2368e1369d4
Viacheslav Shatan <sha02viacheslav@gmail.com>
2021-10-03 09:48:14 +0300

    fix publish forum

--- src/app/modules/coffee-lab/components/publish-forum/publish-forum.component.ts
+++ src/app/modules/coffee-lab/components/publish-forum/publish-forum.component.ts
@@ -1,45 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { CoffeeLabService } from '@services';
 import { DialogService } from 'primeng/dynamicdialog';
 import { SignupModalComponent } from '../signup-modal/signup-modal.component';
-import { takeUntil } from 'rxjs/operators';
-import { DestroyableComponent } from '@base-components';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-publish-forum',
     templateUrl: './publish-forum.component.html',
     styleUrls: ['./publish-forum.component.scss'],
 })
-export class PublishForumComponent extends DestroyableComponent implements OnInit {
+export class PublishForumComponent implements OnInit {
     @Input() type: string;
     firstBtnValue: string;
     secondBtnValue: string;
     placeHolderValue: string;
-    constructor(
-        public dialogSrv: DialogService,
-        private coffeeLabService: CoffeeLabService,
-        private translator: TranslateService,
-    ) {
-        super();
-    }
+    constructor(public dialogSrv: DialogService) {}
 
     ngOnInit(): void {
-        this.coffeeLabService.gotTranslations.pipe(takeUntil(this.unsubscribeAll$)).subscribe((language) => {
-            if (this.type === 'article') {
-                this.firstBtnValue = this.translator.instant('ask_a_question');
-                this.secondBtnValue = this.translator.instant('create_brew_guide');
-                this.placeHolderValue = this.translator.instant('write_post');
-            } else if (this.type === 'recipe') {
-                this.firstBtnValue = this.translator.instant('ask_a_question');
-                this.secondBtnValue = this.translator.instant('write_post');
-                this.placeHolderValue = this.translator.instant('create_brew_guide');
-            } else {
-                this.firstBtnValue = this.translator.instant('write_post');
-                this.secondBtnValue = this.translator.instant('create_brew_guide');
-                this.placeHolderValue = this.translator.instant('ask_your_question');
-            }
-        });
+        if (this.type === 'article') {
+            this.firstBtnValue = 'ask_a_question';
+            this.secondBtnValue = 'create_brew_guide';
+            this.placeHolderValue = 'write_post';
+        } else if (this.type === 'recipe') {
+            this.firstBtnValue = 'ask_a_question';
+            this.secondBtnValue = 'write_post';
+            this.placeHolderValue = 'create_brew_guide';
+        } else {
+            this.firstBtnValue = 'write_post';
+            this.secondBtnValue = 'create_brew_guide';
+            this.placeHolderValue = 'ask_your_question';
+        }
     }
 
     onFocus() {
