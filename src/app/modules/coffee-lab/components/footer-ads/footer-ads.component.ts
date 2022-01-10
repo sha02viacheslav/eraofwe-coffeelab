9a88c4d9ba9a26fe7150a2c10902abcb1f9d7474
Viacheslav Shatan <sha02viacheslav@gmail.com>
2022-01-10 20:18:23 +0530

    UI

---
+++ src/app/modules/coffee-lab/components/footer-ads/footer-ads.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-footer-ads',
+    templateUrl: './footer-ads.component.html',
+    styleUrls: ['./footer-ads.component.scss'],
+})
+export class FooterAdsComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
