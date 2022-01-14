9890182ac6dcafd5bd39ebc770fab66083ad6c7c
Viacheslav Shatan <sha02viacheslav@gmail.com>
2022-01-14 20:12:20 +0530

    UI mob

--- src/app/modules/landing-page/landing-page.component.ts
+++ src/app/modules/landing-page/landing-page.component.ts
@@ -16,18 +16,63 @@ export class LandingPageComponent implements OnInit {
         },
     ];
 
+    shareData = [
+        {
+            heading: 'Create an account on Era of We',
+            disp: 'You can contribute as an estate, roaster, barista, or coffee consumer',
+            image: 'assets/images/group-29.svg',
+        },
+        {
+            heading: 'Write in a language of your choosing',
+            disp: 'We aim to be as inclusive as we can, so you can write in over 10 languages',
+            image: 'assets/images/noun-languages-2711986.svg',
+        },
+        {
+            heading: 'Tell your story- as a brand or coffee lover',
+            disp:
+                'Brands can promote their vision and share knowledge while consumers share their views and discover new brands.',
+            image: 'assets/images/shape-2.svg',
+        },
+    ];
+    hereData = [
+        {
+            name: 'Vasileia Fanarioti',
+            position: 'The Wandering Bean',
+            disp:
+                'Cooking in the heart of Cajun country is an art form. There really is very little science to this particular form of cooking that includes a lot more than mere lagniappe from the pantry or the spice cabinet. Cajun cooking is something that has often been imitated around the country and around the world but can very rarely be accurately duplicated.',
+            image: 'assets/images/rectangle.png',
+        },
+        {
+            name: 'Anna Nordström',
+            position: 'Anna Nordström',
+            disp:
+                'Cooking in the heart of Cajun country is an art form. There really is very little science to this particular form of cooking that includes a lot more than mere lagniappe from the pantry or the spice cabinet. Cajun cooking is something that has often been imitated around the country and around the world but can very rarely be accurately duplicated.',
+            image: 'assets/images/rectangle-2.png',
+        },
+        {
+            name: 'Yker Valerio',
+            position: 'Bon Vivant Caffè',
+            disp:
+                'Cooking in the heart of Cajun country is an art form. There really is very little science to this particular form of cooking that includes a lot more than mere lagniappe from the pantry or the spice cabinet. Cajun cooking is something that has often been imitated around the country and around the world but can very rarely be accurately duplicated.',
+            image: 'assets/images/rectangle-3.png',
+        },
+    ];
+
     platform = [
         {
+            arrow: 'assets/images/arrow-down-circle-2.svg',
             heading: 'Coffee Bussinesses',
             disp:
                 'Share your brand story directly with our audience. This platform gives you the space to build trust in your brand by sharing your expertise with consumers.',
         },
         {
+            arrow: 'assets/images/arrow-down-circle-2.svg',
             heading: 'Consumers',
             disp:
-                'Share your brand story directly with our audience. This platform gives you the space to build trust in your brand by sharing your expertise with consumers.',
+                'Write about your coffee experiences, share your own articles and recipes, pose questions to experts and other consumers. It enables you to meet other coffee lovers from around the world',
         },
         {
+            arrow: 'assets/images/arrow-down-circle.svg',
             heading: 'Proofreaders',
             disp:
                 'Help make coffee more inclusive by translating and proofreading in 9 languages, with more languages being added soon.',
--- src/app/modules/landing-page/landing-page.module.ts
+++ src/app/modules/landing-page/landing-page.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AccordionModule } from 'primeng/accordion';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 
 @NgModule({
     declarations: [LandingPageComponent],
-    imports: [CommonModule, LandingPageRoutingModule],
+    imports: [CommonModule, LandingPageRoutingModule, AccordionModule],
 })
 export class LandingPageModule {}
