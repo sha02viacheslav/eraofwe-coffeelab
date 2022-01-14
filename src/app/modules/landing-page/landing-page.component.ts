ad561939904eefbb39cc981d49919a2b3f814173
Viacheslav Shatan <sha02viacheslav@gmail.com>
2022-01-14 12:32:46 +0530

    UI home

--- src/app/modules/landing-page/landing-page.component.ts
+++ src/app/modules/landing-page/landing-page.component.ts
@@ -15,6 +15,24 @@ export class LandingPageComponent implements OnInit {
             image: 'assets/images/unsplash.png',
         },
     ];
+
+    platform = [
+        {
+            heading: 'Coffee Bussinesses',
+            disp:
+                'Share your brand story directly with our audience. This platform gives you the space to build trust in your brand by sharing your expertise with consumers.',
+        },
+        {
+            heading: 'Consumers',
+            disp:
+                'Share your brand story directly with our audience. This platform gives you the space to build trust in your brand by sharing your expertise with consumers.',
+        },
+        {
+            heading: 'Proofreaders',
+            disp:
+                'Help make coffee more inclusive by translating and proofreading in 9 languages, with more languages being added soon.',
+        },
+    ];
     constructor() {}
 
     ngOnInit(): void {}
