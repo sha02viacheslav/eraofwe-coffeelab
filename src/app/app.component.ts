dbe67412bf31b27c2ce6de6da5a73f5804026ce4
Viacheslav Shatan <sha02viacheslav@gmail.com>
2022-01-19 16:01:28 +0300

    Fix social login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, Injector, PLATFORM_ID } from '@angular/core';
 import { I18NService, SEOService } from '@services';
 import { environment } from '@env/environment';
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { getLangRoute } from '@utils';
+import { SocialAuthService } from 'angularx-social-login';
 
 @Component({
     selector: 'app-root',
@@ -16,6 +17,7 @@ export class AppComponent {
         private i8nService: I18NService,
         @Inject(DOCUMENT) private document: Document,
         @Inject(PLATFORM_ID) private platformId: object,
+        private injector: Injector,
     ) {
         this.seoService.createLinkForCanonicalURL();
         this.document.documentElement.lang = getLangRoute(this.i8nService.currentLang);
@@ -30,25 +32,32 @@ export class AppComponent {
         if (isPlatformBrowser(this.platformId)) {
             setTimeout(
                 () => {
-                    const dynamicScripts = [];
-                    if (environment.production) {
-                        if (environment.needBugherd) {
-                            dynamicScripts.push('https://www.bugherd.com/sidebarv2.js?apikey=y5kbdd1ahghgywnjmyw7lg');
-                        }
-                        dynamicScripts.push(
-                            'https://cmp.osano.com/6olZFSThsdZWqs/d3243605-8fd0-446a-9b25-a172e9ae3d67/osano.js',
-                        );
-                        dynamicScripts.push('https://jscloud.net/x/13391/inlinks.js');
-                        dynamicScripts.push('https://www.googletagmanager.com/gtm.js?id=GTM-KZWL6RR');
-                    }
+                    // Have to create SocialAuthService instance before loading osano
+                    this.injector.get(SocialAuthService);
 
-                    for (const value of dynamicScripts) {
-                        const node = this.document.createElement('script');
-                        node.src = value;
-                        node.type = 'text/javascript';
-                        node.async = true;
-                        this.document.getElementsByTagName('head')[0].appendChild(node);
-                    }
+                    setTimeout(() => {
+                        const dynamicScripts = [];
+                        if (environment.production) {
+                            if (environment.needBugherd) {
+                                dynamicScripts.push(
+                                    'https://www.bugherd.com/sidebarv2.js?apikey=y5kbdd1ahghgywnjmyw7lg',
+                                );
+                            }
+                            dynamicScripts.push(
+                                'https://cmp.osano.com/6olZFSThsdZWqs/d3243605-8fd0-446a-9b25-a172e9ae3d67/osano.js',
+                            );
+                            dynamicScripts.push('https://jscloud.net/x/13391/inlinks.js');
+                            dynamicScripts.push('https://www.googletagmanager.com/gtm.js?id=GTM-KZWL6RR');
+                        }
+
+                        for (const value of dynamicScripts) {
+                            const node = this.document.createElement('script');
+                            node.src = value;
+                            node.type = 'text/javascript';
+                            node.async = true;
+                            this.document.getElementsByTagName('head')[0].appendChild(node);
+                        }
+                    }, 2000);
                 },
                 this.isStaging ? 0 : 7000,
             );
