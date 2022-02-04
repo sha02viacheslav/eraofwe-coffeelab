63179df70327cce06ea5143680f7806c9e8123ea
Viacheslav Shatan <sha02viacheslav@gmail.com>
2022-02-04 20:26:37 +0530

    mailchimp vairable added

--- src/core/services/api/coffee-lab.service.ts
+++ src/core/services/api/coffee-lab.service.ts
@@ -96,7 +96,11 @@ export class CoffeeLabService extends ApiService {
     subscribeToMailList(email: string) {
         const mailChimpEndpoint =
             'https://nordsud.us6.list-manage.com/subscribe/post-json?u=e93a215496f5c5f6203911365&amp;id=f4f7c2427a';
-        const params = new HttpParams().set('EMAIL', email).set('MERGE6', 'sewn-tcl');
+        let key = 'sewn-tcl';
+        if (!environment.needProtect) {
+            key = 'era-tcl';
+        }
+        const params = new HttpParams().set('EMAIL', email).set('MERGE6', key);
         const mailChimpUrl = mailChimpEndpoint + '&' + params.toString();
         return this.http.jsonp(mailChimpUrl, 'c');
     }
