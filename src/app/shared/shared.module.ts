cdf8cd708d4c131935504fab3b7933fccefe31a1
Viacheslav Shatan <sha02viacheslav@gmail.com>
2022-02-10 12:45:39 +0530

    Breadcrum issue fix

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
-import { BreadcrumbModule } from 'primeng/breadcrumb';
 import { EmptyComponent } from './components/empty/empty.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { AmTimeAgoPipe, ConvertToShortDescriptionPipe, CountryPipe, DateAgoReplacePipe, LanguagePipe } from './pipes';
 import { OrgTypePipe } from './pipes/org-type.pipe';
 
-const THIRDMODULES = [LazyLoadImageModule, TranslateModule, BreadcrumbModule];
+const THIRDMODULES = [LazyLoadImageModule, TranslateModule];
 
 const COMPONENTS = [LoadingComponent, EmptyComponent];
 
